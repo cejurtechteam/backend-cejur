521732f555112949d31bd2512393618d29f33675
ynfleozin <leonardoalvarengam@gmail.com>
2025-05-10 11:50:58 -0300

    add initial server configuration

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const articlesRoutes = require("./routes/articles");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/articles", articlesRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servido rodando em na porta ${PORT}`)
+})
