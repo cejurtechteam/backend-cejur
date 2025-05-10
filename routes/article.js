a93c09587fc361e7b6d1189fea026ff85922cb67
ynfleozin <leonardoalvarengam@gmail.com>
2025-05-10 11:53:43 -0300

    add endpoint to return article via ID

--- routes/article.js
+++ routes/article.js
@@ -16,3 +16,17 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Erro ao buscar artigos." });
   }
 });
+
+// GET article by ID
+
+router.get("/:id", async (req, res) => {
+  try {
+    const doc = await db.collection("articles").doc(req.params.id).get();
+    if (!doc.exists) {
+      return res.status(404).json({ error: "Artigo não encontrado" });
+    }
+    res.json({ id: doc.id, ...doc.data() });
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao buscar artigo" });
+  }
+});
